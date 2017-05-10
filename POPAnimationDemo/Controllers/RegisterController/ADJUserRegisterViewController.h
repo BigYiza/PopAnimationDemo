dae24a9b178e64a12e0eca68f4f34f01c2e182a8
zhangyi <zhangyi@danlu.com>
2017-05-10 23:02:01 +0800

    userRegisterViewController 创建

---
+++ POPAnimationDemo/Controllers/RegisterController/ADJUserRegisterViewController.h
@@ -0,0 +1,13 @@
+//
+//  ADJUserRegisterViewController.h
+//  POPAnimationDemo
+//
+//  Created by 张义 on 2017/5/10.
+//  Copyright © 2017年 BearZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ADJUserRegisterViewController : UIViewController
+
+@end
